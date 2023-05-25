3e5fd06782b343f7bd49897247b4eee44176b783
Grigoriy <inpuct@mail.ru>
2023-05-25 14:23:32 +0300

    First Ex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите какое число А вы хотите возвести в степень В ");
+int firstNumber, secondNumber;
+firstNumber = Convert.ToInt32(Console.ReadLine());
+secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вот что вышло {exponentiation(firstNumber, secondNumber)}");
+
+int exponentiation(int number, int degree)
+{
+    int result = number;
+    for (int i = 1; i < degree; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
