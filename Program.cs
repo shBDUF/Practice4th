a44c136f0a24ec58c38267edd262f1cf1580c6c5
Grigoriy <inpuct@mail.ru>
2023-05-25 15:17:58 +0300

    Third Ex

--- Program.cs
+++ Program.cs
@@ -14,16 +14,28 @@
 //     return result;
 // }
 
-Console.WriteLine("Напишите число ");
-int Number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(" сумма цифр в числе {0} равна {1}", Number, SumOfDigit(Number));
-int SumOfDigit(int number)
+// Console.WriteLine("Напишите число ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(" сумма цифр в числе {0} равна {1}", Number, SumOfDigit(Number));
+// int SumOfDigit(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number = number / 10;
+//     }
+//     return result;
+// }
+Console.WriteLine("Введите размерность массива: ");
+Random rnd = new Random();
+int lenghtOfArray = Convert.ToInt32(Console.ReadLine());
+int[] array =new int[lenghtOfArray];
+for (int i = 0; i < lenghtOfArray; i++)
 {
-    int result = 0;
-    while (number > 0)
-    {
-        result += number % 10;
-        number = number / 10;
-    }
-    return result;
+    array[i]= rnd.Next(0,100);
+}
+for (int i = 0; i < lenghtOfArray; i++)
+{
+    Console.WriteLine($"{i} эллемент массива = {array[i]}");
 }
